d5b1a9684a17d65f8e53a913cadf73b824ff741c
vesclop <vesclop2.0@gmail.com>
2021-02-04 18:07:19 +0300

    login

---
+++ src/views/Login.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h2>Войти в аккаунт</h2>
+    <input placeholder="Логин">
+    <br>
+    <input placeholder="Пароль">
+    <br>
+    <button>Войти</button>
+  </div>
+</template>
